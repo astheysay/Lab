af76245d3a86f21dbb82dd1ac04b35f713cab751
astheysay <54431906+astheysay@users.noreply.github.com>
2023-04-04 22:45:30 -0500

    Update EventLogs-S1.ps1

--- EventLogs-S1.ps1
+++ EventLogs-S1.ps1
@@ -1,12 +1,15 @@
+$Source = "C:\windows\system32\winevt\Logs"
+$Destination = "C:\temp"
+
 md -Force c:\temp\
-cp C:\windows\system32\winevt\Logs\System.evtx C:\temp\System.evtx
-cp C:\windows\system32\winevt\Logs\Application.evtx C:\temp\Application.evtx
-cp C:\windows\system32\winevt\Logs\SentinelOne%4Operational.evtx C:\temp\S1-Operational.evtx
+cp $Source\System.evtx $Destination\System.evtx
+cp $Source\Application.evtx $Destination\Application.evtx
+cp $Source\SentinelOne%4Operational.evtx $Destination\S1-Operational.evtx
 
-Compress-Archive -Path C:\temp\System.evtx -Update -DestinationPath c:\temp\EventLogs.zip
-Compress-Archive -Path C:\temp\Application.evtx -Update -DestinationPath c:\temp\EventLogs.zip
-Compress-Archive -Path C:\temp\S1-Operational.evtx -Update -DestinationPath c:\temp\EventLogs.zip
+Compress-Archive -Path $Destination\System.evtx -Update -DestinationPath $Destination\S1-EventLogs.zip
+Compress-Archive -Path $Destination\Application.evtx -Update -DestinationPath $Destination\S1-EventLogs.zip
+Compress-Archive -Path $Destination\S1-Operational.evtx -Update -DestinationPath $Destination\S1-EventLogs.zip
 
-rm c:\temp\Application.evtx -force
-rm c:\temp\System.evtx -force
-rm c:\temp\S1-Operational.evtx -force
+rm $Destination\Application.evtx -force
+rm $Destination\System.evtx -force
+rm $Destination\S1-Operational.evtx -force
